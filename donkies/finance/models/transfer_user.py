a02a11c23a19b0480939e5ec4deea4a6c0bf6625
vladigris <vladigris@gmail.com>
2017-01-19 01:53:52 +0300

    Transfers

--- donkies/finance/models/transfer_user.py
+++ donkies/finance/models/transfer_user.py
@@ -1,32 +1,42 @@
 from django.db import models
 from django.contrib import admin
-from django.utils import timezone
 
 
 class TransferUserManager(models.Manager):
-    def create_transfer(self, account_from, account_to, transaction):
-        t = self.model(
-            account_from=account_from,
-            account_to=account_to,
-            transaction=transaction)
-        t.save()
-        return t
+    pass
 
 
 class TransferUser(models.Model):
-    account_from = models.ForeignKey('Account', related_name='transfers_from')
-    account_to = models.ForeignKey('Account', related_name='transfers_to')
+    """
+    As soon as Donkies LLC received money from user to bank account,
+    process all data from TransferDonkies to TransferUser.
+    Received amount should be splitted between all user's Debt accounts
+    accordingly to share.
+
+    After all data have been processed to TransferUser, send payment to user's
+    debt accounts.
+
+    In current implementations by cheques manually.
+    """
+    account = models.ForeignKey(
+        'Account',
+        related_name='transfers_user',
+        help_text='Debt account.')
+    td = models.ForeignKey(
+        'TransferDonkies', help_text='Related TransferDonkies amount')
+    share = models.IntegerField(help_text='Current share on processing date.')
     amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    dt = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
+    processed_at = models.DateTimeField(null=True, default=None, blank=True)
     is_processed = models.BooleanField(default=False)
 
     objects = TransferUserManager()
 
     class Meta:
         app_label = 'finance'
-        verbose_name = 'transfer'
-        verbose_name_plural = 'transfers'
-        ordering = ['-dt']
+        verbose_name = 'transfer user'
+        verbose_name_plural = 'transfers user'
+        ordering = ['-created_at']
 
     def __str__(self):
         return str(self.id)
@@ -35,9 +45,11 @@ class TransferUser(models.Model):
 @admin.register(TransferUser)
 class TransferUserAdmin(admin.ModelAdmin):
     list_display = (
-        'dt',
-        'account_from',
-        'account_to',
+        'created_at',
+        'account',
+        'td',
+        'share',
         'amount',
+        'processed_at',
         'is_processed'
     )

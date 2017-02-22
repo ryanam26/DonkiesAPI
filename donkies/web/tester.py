d49176aded8bf895785f21031062866c66acc93f
vladigris <vladigris@gmail.com>
2017-02-22 11:00:38 +0300

    tester.py

--- donkies/web/tester.py
+++ donkies/web/tester.py
@@ -18,11 +18,101 @@ class Tester:
         from web.tasks import send_email
         send_email()
 
-    def get_atrium_users(self):
+    def clean(self):
+        """
+        Accurate!!! Clean all except that are in emails list.
+        """
+        return
+        # from finance.services.atrium_api import AtriumApi
+        # User = apps.get_model('web', 'User')
+
+        # emails = ['alex@donkies.co', 'a@a.com']
+        # good_guids = User.objects.filter(
+        #     email__in=emails).values_list('guid', flat=True)
+
+        # a = AtriumApi()
+        # for user in a.get_users():
+        #     if user.guid not in good_guids:
+        #         a.delete_user(user.guid)
+
+        # User.objects.filter(~Q(email__in=emails)).delete()
+
+    def get_users(self):
+        """
+        Returns all Atrium users.
+        """
         from finance.services.atrium_api import AtriumApi
 
         a = AtriumApi()
         return a.get_users()
+
+    def get_lost_users(self):
+        """
+        Returns users that exist in Atrium and doesn't exist
+        in database.
+        """
+        User = apps.get_model('web', 'User')
+
+        qs = User.objects.filter(is_admin=False).values_list('guid', flat=True)
+        guids = list(qs)
+        guids = [guid for guid in guids if guid is not None]
+
+        return [d for d in self.get_users() if d.guid not in guids]
+
+    def print_users(self):
+        for d in self.get_users():
+            print(d)
+
+    def print_lost_users(self):
+        l = self.get_lost_users()
+        if not l:
+            print('No lost users')
+            return
+
+        for d in l:
+            print(d)
+
+    def get_members(self, user_id):
+        """
+        Returns all Atrium members for particular user.
+        """
+        from finance.services.atrium_api import AtriumApi
+        User = apps.get_model('web', 'User')
+        user = User.objects.get(id=user_id)
+        a = AtriumApi()
+        return a.get_members(user.guid)
+
+    def get_lost_members(self, user_id):
+        """
+        Returns all members that exist in Atrium and doesn't exist in
+        database.
+        """
+        Member = apps.get_model('finance', 'Member')
+        qs = Member.objects.filter(
+            user_id=user_id).values_list('guid', flat=True)
+        guids = list(qs)
+        qs = Member.objects.filter(user_id=user_id)
+        return [d for d in self.get_members(user_id) if d.guid not in guids]
+
+    def print_members(self):
+        """
+        Prints Atrium members for all users.
+        """
+        User = apps.get_model('web', 'User')
+        for user in User.objects.filter(is_admin=False):
+            print('\n------- User: {}'.format(user.email))
+            for d in self.get_members(user.id):
+                print(d)
+
+    def print_lost_members(self):
+        """
+        Prints lost members for all users.
+        """
+        User = apps.get_model('web', 'User')
+        for user in User.objects.filter(is_admin=False):
+            print('\n------- User: {}'.format(user.email))
+            for d in self.get_lost_members(user.id):
+                print(d)
 
     def resume_halted(self):
         from finance.services.atrium_api import AtriumApi
@@ -35,24 +125,8 @@ class Tester:
                 if member.status == Member.HALTED:
                     a.aggregate_member(member.user_guid, member.guid)
 
-    def clean(self):
-        """
-        Accurate!!!
-        """
-        return
-        from finance.services.atrium_api import AtriumApi
-        User = apps.get_model('web', 'User')
-
-        emails = ['alex@donkies.co', 'a@a.com']
-        good_guids = User.objects.filter(
-            email__in=emails).values_list('guid', flat=True)
-
-        a = AtriumApi()
-        for user in a.get_users():
-            if user.guid not in good_guids:
-                a.delete_user(user.guid)
-
-        User.objects.filter(~Q(email__in=emails)).delete()
 
 if __name__ == '__main__':
     t = Tester()
+    t.print_members()
+    # t.print_lost_members()

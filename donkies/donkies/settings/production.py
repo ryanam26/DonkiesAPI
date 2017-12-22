a665acd8693893e0836ef7d8ab798308b58af08c
Alexander Ksenzov <alexander.ksenzov@gmail.com>
2017-12-22 12:03:24 +0200

    Change Plaid environment to developmentx

--- donkies/donkies/settings/production.py
+++ donkies/donkies/settings/production.py
@@ -3,7 +3,8 @@ from .base import *
 DEBUG = False
 PRODUCTION = True
 TESTING = False
-# PLAID_ENV = 'development'
-PLAID_ENV = 'production'
+PLAID_ENV = 'development'
+# PLAID_ENV = 'sandbox'
+# PLAID_ENV = 'production'
 DWOLLA_ENV = 'sandbox'
 DONKIES_MODE = 'production'
